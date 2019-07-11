6659f1b6a642e664cd513d29cd81a8a4a20974c1
PyYoshi <myoshi321go@gmail.com>
2019-07-11 19:51:10 +0900

    update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ mkdir -p /secret
 echo -e $HTPASSWD > /secret/.htpasswd
 echo -e $TLS_PEM > /secret/tls.pem
 echo -e $TLS_KEY > /secret/tls.key
+echo -e $TRUSTED_CA > /secret/trusted.crt
 echo -e $DHPARAM_PEM > /secret/dhparam.pem
 
 nginx -c /etc/nginx/conf.d/development.conf -t
